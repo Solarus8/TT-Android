4de0f6e910159d4aa0254954fb981627a176c2e1
Tom Anderson <immranderson@gmail.com>
2013-12-31 00:45:19 -0800

    Google Maps with Hamburger Menu

--- TerraTraveler/src/com/metapresence/android/terratraveler/MainFragment.java
+++ TerraTraveler/src/com/metapresence/android/terratraveler/MainFragment.java
@@ -1,23 +1,17 @@
 package com.metapresence.android.terratraveler;
 
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.util.ResourceProxyImpl;
-import org.osmdroid.views.MapView;
 
-import android.R.integer;
 import android.app.Fragment;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.MapView;
+
 public class MainFragment extends Fragment {
 	
 	private MapView mMapView;
-	private ResourceProxyImpl mResourceProxyImpl;
-	private MyItemizedOverlay mItemizedOverlay = null;
-	
 	
 	
 	@Override
@@ -26,23 +20,23 @@ public class MainFragment extends Fragment {
 		
 		setRetainInstance(true);
 		
-		mResourceProxyImpl = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
-		mMapView = new MapView(inflater.getContext(), 256, mResourceProxyImpl);
-		
-		mMapView.setUseSafeCanvas(true);
-		mMapView.setBuiltInZoomControls(true);
-		mMapView.setMultiTouchControls(true);
-		
-		Drawable marker = getResources().getDrawable(R.drawable.ic_action_person);
-		int marker_width = marker.getIntrinsicWidth();
-		int marker_height = marker.getIntrinsicHeight();
-		
-		marker.setBounds(0, marker_height, marker_width, 0);
-		mItemizedOverlay = new MyItemizedOverlay(marker, mResourceProxyImpl);
-		mMapView.getOverlays().add(mItemizedOverlay);
-		
-		GeoPoint point1 = new GeoPoint(0*1000000, 0*1000000);
-		mItemizedOverlay.addItem(point1, "Point1", "Point1");
+//		mResourceProxyImpl = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+//		mMapView = new MapView(inflater.getContext(), 256, mResourceProxyImpl);
+//		
+//		mMapView.setUseSafeCanvas(true);
+//		mMapView.setBuiltInZoomControls(true);
+//		mMapView.setMultiTouchControls(true);
+//		
+//		Drawable marker = getResources().getDrawable(R.drawable.ic_action_person);
+//		int marker_width = marker.getIntrinsicWidth();
+//		int marker_height = marker.getIntrinsicHeight();
+//		
+//		marker.setBounds(0, marker_height, marker_width, 0);
+//		mItemizedOverlay = new MyItemizedOverlay(marker, mResourceProxyImpl);
+//		mMapView.getOverlays().add(mItemizedOverlay);
+//		
+//		GeoPoint point1 = new GeoPoint(0*1000000, 0*1000000);
+//		mItemizedOverlay.addItem(point1, "Point1", "Point1");
 		
 		
 		return mMapView;
